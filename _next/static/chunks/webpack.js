272988b2f03503443cb41ea5230ed787188753a0
Pavlo Getta <pavlo.getta@epfl.ch>
2021-04-07 18:26:18 +0200

    Add cookies policy

--- _next/static/chunks/webpack.js
+++ _next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "c0c9dff72ce2a8892c21";
+/******/ 	var hotCurrentHash = "8d5c440cbbeae1ec356b";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
