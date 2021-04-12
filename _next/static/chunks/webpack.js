77fa8d966cbfb628fa3cc727b215071d9f0da711
Pavlo Getta <pavlo.getta@epfl.ch>
2021-04-12 12:06:51 +0200

    Add Sentry

--- _next/static/chunks/webpack.js
+++ _next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "773b009daadbd9950a3e";
+/******/ 	var hotCurrentHash = "acfe7c6fbdf50c966773";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
