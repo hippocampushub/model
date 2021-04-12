c508388e61bc9115dada7a94bde0c82d3b8f1076
Pavlo Getta <pavlo.getta@epfl.ch>
2021-04-12 21:25:01 +0200

    Deploy latest changes

--- _next/static/chunks/webpack.js
+++ _next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "acfe7c6fbdf50c966773";
+/******/ 	var hotCurrentHash = "f6fc5808c61b2c9a91f9";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
