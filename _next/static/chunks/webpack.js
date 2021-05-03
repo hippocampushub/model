47d765fcf8ae2ac38a347dc1d9e4fe029725ab49
Pavlo Getta <pavlo.getta@epfl.ch>
2021-05-03 11:08:36 +0200

    Revert "Deploy latest changes"
    This reverts commit 28ccf77b8e0d216b1c8418e6a3b15e277a1433d0.

--- _next/static/chunks/webpack.js
+++ _next/static/chunks/webpack.js
@@ -182,7 +182,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "de3ea328aacdf9b9249c"; }
+/******/ 		__webpack_require__.h = function() { return "21c0a1c6e9211df65ea5"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
