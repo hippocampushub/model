c8184068395b1df62c13ef63da20939731fa0158
Pavlo Getta <pavlo.getta@epfl.ch>
2021-06-08 08:03:44 +0200

    Add missing styles for the Feedback component

--- _next/static/chunks/webpack.js
+++ _next/static/chunks/webpack.js
@@ -182,7 +182,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "75991996b0129c1190d8"; }
+/******/ 		__webpack_require__.h = function() { return "26e1ecc16a06f0ef6407"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
