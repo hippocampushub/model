e205075dd95a012c0dacc826dd2701d610d08256
Pavlo Getta <pavlo.getta@epfl.ch>
2021-06-14 07:27:39 +0200

    Update menu

--- _next/static/chunks/webpack.js
+++ _next/static/chunks/webpack.js
@@ -182,7 +182,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "26e1ecc16a06f0ef6407"; }
+/******/ 		__webpack_require__.h = function() { return "9fae939a78be0d1cad0f"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
