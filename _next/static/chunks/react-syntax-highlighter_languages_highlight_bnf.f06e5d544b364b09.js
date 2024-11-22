9e831001c81f417fc67f3089ab52eb12ff401564
Pavlo Getta <pavlo.getta@epfl.ch>
2024-11-22 17:41:15 +0100

    Post release fixes

---
+++ _next/static/chunks/react-syntax-highlighter_languages_highlight_bnf.f06e5d544b364b09.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[9346],{78349:function(n){n.exports=function(n){return{name:"Backus–Naur Form",contains:[{className:"attribute",begin:/</,end:/>/},{begin:/::=/,end:/$/,contains:[{begin:/</,end:/>/},n.C_LINE_COMMENT_MODE,n.C_BLOCK_COMMENT_MODE,n.APOS_STRING_MODE,n.QUOTE_STRING_MODE]}]}}}}]);
